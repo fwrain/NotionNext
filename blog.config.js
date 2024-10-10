9a20f1aba9265aa8816f6f6b81d046711b3214db
rain <152087224+fwrain@users.noreply.github.com>
2024-10-10 18:59:23 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'rain', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'ref ^^', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'wuwuwu', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || ' 随笔小记', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
